5a8d4804735a46fd4c8285d262562d85a0602c30
rwitzel <rodrigo.witzel@gmx.de>
2021-05-09 22:20:09 +0200

    implemented: UI - part X

--- spanish_nouns_downloader/download_spanish_nouns.sh
+++ spanish_nouns_downloader/download_spanish_nouns.sh
@@ -3,6 +3,7 @@
 # call it from the project's root folder
 curl --output es-en.xml https://raw.githubusercontent.com/mananoreboton/en-es-en-Dic/master/src/main/resources/dic/es-en.xml
 
-echo "const spanish_nouns = " > spanish_nouns.js
-cat es-en.xml | xq '.dic.l[0].w[] | select( (.t | startswith("{f}") ) or (.t | startswith("{m}")) ) | .c' | jq -s >> spanish_nouns.js
-echo ";" >> spanish_nouns.js
+FILE=frontend/01_introduction/spanish_nouns.js
+echo "const spanish_nouns = " > $FILE
+cat es-en.xml | xq '.dic.l[0].w[] | select( (.t | startswith("{f}") ) or (.t | startswith("{m}")) ) | .c' | jq -s >> $FILE
+echo ";" >> $FILE
