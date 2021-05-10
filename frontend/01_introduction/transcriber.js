807abbcdc928d4e38a4cd3bcc12f6a559742a8e7
rwitzel <rodrigo.witzel@gmx.de>
2021-05-10 13:14:36 +0200

    implemented: UI - part X

--- frontend/01_introduction/transcriber.js
+++ frontend/01_introduction/transcriber.js
@@ -1,10 +1,16 @@
 app.controller('transcriberCtrl', ($scope) => {
     $scope.params = {
         "spanish_text" : "hoven aventura vender colegas",
-        "rules": rules
+        "rules": rules,
+        "rules_filter": ""
     };
     $scope.internol_text = "";
     $scope.applied_rules = {};
+    $scope.filter_rules = (rules, filter) => {
+        return rules.filter((rule) => {
+            return JSON.stringify(rule).indexOf(filter) >=0;
+        });
+    };
 
     const on_params_change = function() {
         const result = to_internol_text($scope.params.spanish_text, $scope.params.rules);
