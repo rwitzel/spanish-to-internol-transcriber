3e20fbe50337bd6cb1e42453849b7592fefff38c
rwitzel <rodrigo.witzel@gmx.de>
2021-05-10 21:47:50 +0200

    implemented: UI - part X

--- spanish_nouns_downloader/download_spanish_nouns.sh
+++ spanish_nouns_downloader/download_spanish_nouns.sh
@@ -2,9 +2,14 @@
 
 # call it from the project's root folder
 # it contains only 2392 nouns, use a better source, like MCR (using WordNet 3.0 as ILI)
-curl --output es-en.xml https://raw.githubusercontent.com/mananoreboton/en-es-en-Dic/master/src/main/resources/dic/es-en.xml
+#curl --output es-en.xml https://raw.githubusercontent.com/mananoreboton/en-es-en-Dic/master/src/main/resources/dic/es-en.xml
 
 FILE=frontend/01_introduction/spanish_nouns.js
 echo "const spanish_nouns = " > $FILE
 cat es-en.xml | xq '.dic.l[].w[] | select( (.t | startswith("{f}") ) or (.t | startswith("{m}")) ) | .c' | jq -s >> $FILE
+echo ";" >> $FILE
+
+FILE=frontend/01_introduction/spanish_adjectives.js
+echo "const spanish_adjectives = " > $FILE
+cat es-en.xml | xq '.dic.l[].w[] | select( (.t | startswith("{adj}") ) ) | .c' | jq -s >> $FILE
 echo ";" >> $FILE
